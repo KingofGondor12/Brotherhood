92a6786435745a1871cf1e20a2b9288d83628540
Chris Hayward <30640240+KingofGondor12@users.noreply.github.com>
2017-09-12 11:17:44 +1000

    Upload Versions

---
+++ brotherhood_login_v1.01.rb
@@ -0,0 +1,29 @@
+def cls
+  system "cls"
+end
+
+puts "Please Log In"
+puts "Username:"
+@username = $stdin.gets.chomp.to_s
+cls
+puts "Password:"
+@password = $stdin.gets.chomp.to_s
+cls
+
+if @username == "J.Kanus"
+  if @password == "password"
+    cls
+    puts "Welcome To The Brotherhood"
+  else
+    cls
+    puts "Incorrect Password"
+  end
+elsif @username == "J.Berg"
+  if @password == "password"
+    cls
+    puts "Welcome To Abstergo Industries"
+  else
+    cls
+    puts "Incorrect Password"
+  end
+end
